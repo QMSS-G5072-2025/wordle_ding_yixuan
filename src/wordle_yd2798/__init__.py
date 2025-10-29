acfbdae87523cb3222bb36ab007e8b23b183b50d
Yixuan Ding <yd2798@columbia.edu>
2025-10-29 15:06:33 -0400

    Updated __init__.py to export functions

--- src/wordle_yd2798/__init__.py
+++ src/wordle_yd2798/__init__.py
@@ -1,3 +1,15 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("wordle_yd2798")
+from .wordle_yd2798 import (
+    validate_guess,
+    check_guess,
+    is_valid_word,
+    calculate_game_score,
+)
+
+__all__ = [
+    "validate_guess",
+    "check_guess",
+    "is_valid_word",
+    "calculate_game_score",
+]
+
+__version__ = "0.1.0"
